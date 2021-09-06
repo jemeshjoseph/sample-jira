9234e0bf43bc08a33b2a214dd4610e0a8a060086
Jemesh Joseph <jemesh.joseph@ust-global.com>
2021-09-06 17:19:21 +0530

    Jira single task drag and drop

---
+++ main.js
@@ -0,0 +1,49 @@
+let task = document.querySelector('.task');
+let taskSections = document.querySelectorAll('.task-section');
+
+// Drag start and Drag end events for the draggable task element.
+task.addEventListener('dragstart', dragStart);
+task.addEventListener('dragend', dragEnd);
+
+function dragStart(e) {
+  console.log('drag start');
+  this.className += ' hold';
+  setTimeout(() => {
+    this.className = 'invisible';
+  }, 0);
+}
+
+function dragEnd(e) {
+  console.log('drag end');
+  this.className = 'task';
+}
+
+// Drag over, Drag enter, Drag leave and Drop events for the task sections.
+taskSections.forEach(section => {
+  section.addEventListener('dragover', dragOver);
+  section.addEventListener('dragenter', dragEnter);
+  section.addEventListener('dragleave', dragLeave);
+  section.addEventListener('drop', dragDrop);
+});
+
+function dragOver(e) {
+  e.preventDefault();
+  console.log('drag over');
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  console.log('drag enter');
+  this.className += ' hovered';
+}
+
+function dragLeave() {
+  console.log('drag leave');
+  this.className = 'task-section';
+}
+
+function dragDrop() {
+  console.log('drop');
+  this.className = 'task-section';
+  this.appendChild(task);
+}
